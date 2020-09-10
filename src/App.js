882c197e5afb3f9955b6f2aec185d431bcaa3e81
Soda-666 <s6135512029@phuket.psu.ac.th>
2020-09-10 12:25:00 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import CharacterCard from './CharacterCard';
+const word = "Hello";
 function App() {
-  return (
-  <div>
-    Hello World
-  </div>
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
  );
-
 }
+
 
 export default App;
